9651df3fc6b10f7a99e77e1be8d068adc4ba7eaf
Ruslan Anisimov <onlylucknplm@gmail.com>
2019-04-18 15:42:10 +0300

    vrode rabotaet :D

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
 
+    // toggle map visibility
     $("#toggle-map").click(function(){
        $(".target-map").slideToggle( '8000');
     });
-
+    // toggle contact form visibility
     $("#toggle-form").click(function(){
         $(".target-form").slideToggle( '8000');
     });
+
+    // hide one on click 
+    $(document).on("click", "#toggle-map", function(event){
+            $(".target-form").hide();
+    });
+
+    $(document).on("click", "#toggle-form", function(event){
+        $(".target-map").hide();
+});
+
  });
