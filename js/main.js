3e6cd6f3cdb33cdebeb6e530c4349ca6f3367476
Ruslan Anisimov <onlylucknplm@gmail.com>
2019-04-18 15:30:39 +0300

    vrode rabotaet :D

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $("#toggle-map").click(function(){
+       $(".target-map").slideToggle( '8000');
+    });
+
+    $("#toggle-form").click(function(){
+        $(".target-form").slideToggle( '8000');
+    });
+ });
