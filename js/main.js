1f07755d0634f28887195a160524f94956c847fc
Ruslan Anisimov <onlylucknplm@gmail.com>
2019-04-19 15:48:48 +0300

    animation almost done

--- js/main.js
+++ js/main.js
@@ -2,20 +2,52 @@ $(document).ready(function() {
 
     // toggle map visibility
     $("#toggle-map").click(function(){
-       $(".target-map").slideToggle( '8000');
+        $(".target-map").toggle( 'slide');
     });
     // toggle contact form visibility
     $("#toggle-form").click(function(){
-        $(".target-form").slideToggle( '8000');
+        $(".target-form").toggle( 'slide');
     });
 
     // hide one on click 
     $(document).on("click", "#toggle-map", function(event){
             $(".target-form").hide();
+            event.preventDefault();
+            if ( $(this).hasClass("isDown") ) {
+                $(".left-part").stop().animate({left:"0px"}, 100);                            
+            } else {
+                $(".left-part").stop().animate({left:"-245px"}, 100);
+            }
+            $(this).toggleClass("isDown");
+            return false;
     });
 
     $(document).on("click", "#toggle-form", function(event){
         $(".target-map").hide();
-});
+        event.preventDefault();
+        if ( $(this).hasClass("isDown") ) {
+            $(".left-part").stop().animate({left:"0px"}, 100);                            
+        } else {
+            $(".left-part").stop().animate({left:"-245px"}, 100);
+        }
+        $(this).toggleClass("isDown");
+        return false;
+    });
+
+    // $("button").click( function(event){
+    //     event.preventDefault();
+    //     if ( $(this).hasClass("isDown") ) {
+    //         $(".left-part").stop().animate({marginRight:"-0px"}, 200);                            
+    //     } else {
+    //         $(".left-part").stop().animate({marginRight:"200px"}, 200);
+    //     }
+    //     $(this).toggleClass("isDown");
+    //     return false;
+    // });
+    
+    // $("button").click(function(){
+    //     $(".left-part").animate({left: '-250px'});
+    //   }); 
 
  });
+
